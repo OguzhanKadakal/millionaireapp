773b1707a5dbf53111b1b5de54787cd1e266d9ec
OguzhanKadakal <76901135+OguzhanKadakal@users.noreply.github.com>
2025-03-18 02:12:25 +0300

    add Trivia component and integrate it into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./app.css"
+import Trivia from "./component/Trivia";
 
 function App() {
   const [questionNumber, setQuestionNumber] = useState(1);
@@ -26,7 +27,9 @@ function App() {
         <div className="top">
           <div className="timer">30</div>
         </div>
-        <div className="bottom">questions and answers</div>
+        <div className="bottom">
+          <Trivia />
+        </div>
       </div>
       <div className="pyramid">
         <ul className="moneyList">
